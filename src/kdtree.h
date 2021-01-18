3521fc96fc90262d42909841e9b7a0395ebb0df3
yumcyawiz <hashdolf1923@gmail.com>
2021-01-19 02:00:56 +0900

    separate point.h from kdtree.h

--- src/kdtree.h
+++ src/kdtree.h
@@ -1,45 +1,12 @@
 #ifndef _KDTREE_H
 #define _KDTREE_H
 #include <algorithm>
-#include <array>
-#include <initializer_list>
 #include <iostream>
 #include <vector>
 
+#include "point.h"
+
 namespace kdtree {
-
-template <typename T, unsigned int N>
-class Point {
- private:
-  std::array<T, N> elements;
-
- public:
-  Point() : Point(0) {}
-  Point(T x) {
-    for (int i = 0; i < N; ++i) {
-      elements[i] = x;
-    }
-  }
-  Point(std::initializer_list<T> init) {
-    if (init.size() > N) {
-      std::cerr << "invalid dimension" << std::endl;
-      std::exit(EXIT_FAILURE);
-    }
-    std::copy(init.begin(), init.end(), elements.begin());
-  }
-
-  T operator[](unsigned int i) const { return elements.at(i); }
-};
-
-using Point2u = Point<unsigned int, 2>;
-using Point2i = Point<int, 2>;
-using Point2f = Point<float, 2>;
-using Point2d = Point<double, 2>;
-
-using Point3u = Point<unsigned int, 3>;
-using Point3i = Point<int, 3>;
-using Point3f = Point<float, 3>;
-using Point3d = Point<double, 3>;
 
 template <typename T>
 struct Node {
