59a5bf00fa55d3df14eb76b1ea1d58a24a8515f8
yumcyawiz <hashdolf1923@gmail.com>
2021-01-19 01:20:42 +0900

    test building kd-tree

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,39 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <random>
+
+#include "kdtree.h"
+
+float rnd() {
+  static std::mt19937 mt(0);
+  static std::uniform_real_distribution<float> dist(0, 1);
+  return dist(mt);
+}
 
 int main() {
-  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-  sf::CircleShape shape(100.0f);
-  shape.setFillColor(sf::Color::Green);
+  const int width = 512;
+  const int height = 512;
+  const int n_balls = 10;
+
+  sf::RenderWindow window(sf::VideoMode(width, height), "SFML works!");
+
+  std::vector<kdtree::Point2f> points;
+  for (int i = 0; i < n_balls; ++i) {
+    points.push_back(kdtree::Point2f{width * rnd(), height * rnd()});
+  }
+
+  kdtree::KdTree<float, 2> tree(points);
+  tree.buildTree();
+
+  std::vector<sf::CircleShape> circles;
+  for (int i = 0; i < n_balls; ++i) {
+    sf::CircleShape circle;
+    circle.setFillColor(sf::Color::Black);
+    circle.setRadius(10.0f);
+    circle.setPosition(sf::Vector2f(points[i][0], points[i][1]));
+
+    circles.push_back(circle);
+  }
 
   while (window.isOpen()) {
     sf::Event event;
@@ -11,8 +41,10 @@ int main() {
       if (event.type == sf::Event::Closed) window.close();
     }
 
-    window.clear();
-    window.draw(shape);
+    window.clear(sf::Color::White);
+    for (const auto& c : circles) {
+      window.draw(c);
+    }
     window.display();
   }
 
